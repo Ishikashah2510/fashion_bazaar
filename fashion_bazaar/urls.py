552ddb8ddc42cd9aa21acbfe8144816a4da52f79
ishika <ishikaishu2000@gmail.com>
2021-01-05 11:45:25 +0530

    basic homepage

--- fashion_bazaar/urls.py
+++ fashion_bazaar/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from home import views as hv
@@ -24,4 +25,4 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', hv.home_page, name='homepage'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
